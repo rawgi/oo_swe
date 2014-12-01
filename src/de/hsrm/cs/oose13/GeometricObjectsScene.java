e6fa66ed2dd1be6125cea26d9b87757fe86b97d8
rawgi <mattes.daniel93@gmx.de>
2014-12-01 17:30:08 +0100

    Update GeometricObjectsScene.java

--- src/de/hsrm/cs/oose13/GeometricObjectsScene.java
+++ src/de/hsrm/cs/oose13/GeometricObjectsScene.java
@@ -3,24 +3,26 @@ package de.hsrm.cs.oose13;
 public class GeometricObjectsScene implements CollisionScene{
   
   private Array(GeometricObject) myObjects;
+  private int width;
+  private int height;
   
-  public GeometricObjectsScene(Array(GeometricObject) objects){
+  public GeometricObjectsScene(Array(GeometricObject) objects, int width, int height){
     myObjects = objects;
+    setWidth(width);
+    setHeight(height);
   }
   
 	public void collisions(){
 	  for(int i: myObjects){
 	    GeometricObject obj = myObjects.get(i);
-	    for(int j: myObjects){
+	    for(int j = i+1; j <= myObjects; j++){
 	      GeometricObject check = myObjects.get(j);
-	      if(!obj.equals(check)){
-  	      if(obj.touches(check)){
-            myObjects.get(i).invertXMovement();
-            myObjects.get(i).invertYMovement();
-            myObjects.get(j).invertXMovement();
-            myObjects.get(j).invertYMovement();
+  	      	if(obj.touches(check)){
+        		myObjects.get(i).invertXMovement();
+            		myObjects.get(i).invertYMovement();
+        		myObjects.get(j).invertXMovement();
+            		myObjects.get(j).invertYMovement();
 	        }
-	      }
 	    }
 	    if(obj.getCorner().getX()<=0 || obj.getCorner().getX()+obj.getWidth()>=getWidth()){
 	      myObjects.get(i).invertXMovement();
@@ -43,11 +45,29 @@ public class GeometricObjectsScene implements CollisionScene{
 	  }
 	}
 	
+	//Getter
 	public int getWidth(){
-	  
+	  return this.width;
 	}
 	
 	public int getHeight(){
-	  
+	  return this.height;
 	}
+	
+	//Setter
+    public void setHeight(int height){
+    	if(height >= 0){
+    		this.height = height;
+    	}else{
+    		throw new IllegalArgumentException("Height must be 0 or higher.");
+    	}
+    }
+    
+    public void setWidth(int width){
+    	if(width >= 0){
+    		this.width = width;
+    	}else{
+    		throw new IllegalArgumentException("Width must be 0 or higher.");
+    	}
+    }
 }
