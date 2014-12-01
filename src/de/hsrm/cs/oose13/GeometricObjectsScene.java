d2f7c3be87fdf034283dc0f852fa6ae42dbbc3b2
rawgi <mattes.daniel93@gmx.de>
2014-12-01 17:13:39 +0100

    Update GeometricObjectsScene.java

--- src/de/hsrm/cs/oose13/GeometricObjectsScene.java
+++ src/de/hsrm/cs/oose13/GeometricObjectsScene.java
@@ -1,10 +1,53 @@
 package de.hsrm.cs.oose13;
 
 public class GeometricObjectsScene implements CollisionScene{
-
-
-
-
-
-
+  
+  private Array(GeometricObject) myObjects;
+  
+  public GeometricObjectsScene(Array(GeometricObject) objects){
+    myObjects = objects;
+  }
+  
+	public void collisions(){
+	  for(int i: myObjects){
+	    GeometricObject obj = myObjects.get(i);
+	    for(int j: myObjects){
+	      GeometricObject check = myObjects.get(j);
+	      if(!obj.equals(check)){
+  	      if(obj.touches(check)){
+            myObjects.get(i).invertXMovement();
+            myObjects.get(i).invertYMovement();
+            myObjects.get(j).invertXMovement();
+            myObjects.get(j).invertYMovement();
+	        }
+	      }
+	    }
+	    if(obj.getCorner().getX()<=0 || obj.getCorner().getX()+obj.getWidth()>=getWidth()){
+	      myObjects.get(i).invertXMovement();
+	    }
+	    if(obj.getCorner().getY()<=0 || obj.getCorner().getY()+obj.getHeight()>=getHeight()){
+	      myObjects.get(i).invertYMovement();
+	    }
+	  }
+	}
+	
+	public void move(){
+	  for(int i=0; i<=myObjects.length(); i++){
+	    myObjects.get(i).move;
+	  }
+	}
+	
+	public void paintAll(Graphics g){
+	  for(int i=0; i<=myObjects.length(); i++){
+	    myObjects.get(i).paintMeTo(g);
+	  }
+	}
+	
+	public int getWidth(){
+	  
+	}
+	
+	public int getHeight(){
+	  
+	}
 }
