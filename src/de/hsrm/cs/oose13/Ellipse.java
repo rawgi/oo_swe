7c16006754e4d62e90b177de94364552f51ba8d2
rawgi <mattes.daniel93@gmx.de>
2014-12-01 16:32:36 +0100

    Update Ellipse.java

--- src/de/hsrm/cs/oose13/Ellipse.java
+++ src/de/hsrm/cs/oose13/Ellipse.java
@@ -4,26 +4,26 @@ import java.awt.Graphics;
 
 public class Ellipse extends GeometricObject{
 
-	public Ellipse(Vertex corner, double width, double height) {
-		super(corner, width, height);
-	}
+   public Ellipse(Vertex corner, double width, double height) {
+ 	super(corner, width, height);
+   }
 	
-	public Ellipse(Vertex corner, Vertex movement, double width, double height) {
-		super(corner, movement, width, height);
-	}
+    public Ellipse(Vertex corner, Vertex movement, double width, double height) {
+	super(corner, movement, width, height);
+    }
 	
-	@Override
+    @Override
     public String toString(){
         return "Ellipse mit: obere linke Ecke: "+corner.toString()+", Breite: "+width+", Hoehe: "+height+", movement: "+movement.toString()+"";
     }
     
-	@Override
+    @Override
     public double size(){
     	return Math.PI*(width/2)*(height/2);
     }
 	
-	@Override
-	public void paintMeTo(Graphics g) {
-		g.drawOval((int)corner.getX(), (int)corner.getY(), (int)width, (int)height);
-	}
+    @Override
+    public void paintMeTo(Graphics g) {
+	g.drawOval((int)corner.getX(), (int)corner.getY(), (int)width, (int)height);
+    }
 }
